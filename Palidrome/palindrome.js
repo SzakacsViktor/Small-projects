3be804bcd87b64d3f65a171ee3340c97331ed132
Szakács Viktor <szavi78@gmail.com>
2018-06-05 22:04:23 +0200

    Check if a typed text is palindrome
    Enter text in a html form, check in js in an other file, and show the result in a div in html.

---
+++ Palidrome/palindrome.js
@@ -0,0 +1,33 @@
+﻿function teszt() {
+
+var palindrome=(document.beker.palindrome.value);
+
+var back=new String();
+
+　
+
+for (var count=palindrome.length-1; count >=0; count--) {
+
+back=back+palindrome.charAt(count);
+
+}
+
+　
+
+palindrome=palindrome.toLowerCase();
+
+back=back.toLowerCase();
+
+　
+
+if (back==palindrome) {
+
+document.getElementById('keret1').innerHTML ='Yes! It is a palindrome.';
+
+} else {
+
+document.getElementById('keret1').innerHTML ='No! It is NOT a palindrome.<br>';
+
+}
+
+}
