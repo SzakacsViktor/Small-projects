b1d97e597d16ed9a47e8d2ba5338989766c0cbee
Szakács Viktor <szavi78@gmail.com>
2018-07-22 21:38:20 +0200

    Drawind with JS
    Next inset delay to the loop

---
+++ JS drawing/jsdrawing.js
@@ -0,0 +1,27 @@
+﻿function draw() {
+  
+  var c=document.getElementById("myCanvas");
+  var ctx=c.getContext("2d");
+
+  ctx.rect(200,200,200,200);
+  ctx.stroke();
+  
+  /*ctx.translate(300, 300);
+  ctx.rotate(45*Math.PI/180);
+  ctx.rect(-100,-100,200,200);
+  ctx.stroke();*/
+  
+  ctx.translate(300, 300);
+  for (var i=1; i<60; i++) {
+    ctx.rotate(i*6*Math.PI/180);
+    ctx.rect(-100,-100,200,200);
+    ctx.stroke();
+} 
+  
+  
+}
+
+
+  
+
+  
