53f47945199ad791d58c4cffdb08966088cb49dc
Szakács Viktor <szavi78@gmail.com>
2018-09-23 15:59:22 +0200

    JS exercises

--- JSRace/Sheets On Sheet/sheet.js
+++ JSRace/Sheets On Sheet/sheet.js
@@ -1,17 +1,14 @@
-function counting() {
+﻿function counting() {
 	var A = 40;
 	var B = 50;
+	var a = document.getElementById("numA").value;
+	var b = document.getElementById("numB").value;
 	
-	var a =Math.floor(A / document.getElementById("numA").value);
-	var b =Math.floor(B / document.getElementById("numB").value);
-	
-	alert((a * b) + " kisebb négyszög fér el a nagyon.");
-	
-	a =Math.floor(B / document.getElementById("numA").value);
-	b =Math.floor(A / document.getElementById("numB").value);
-	
-	alert("Ha a lapot 90 fokban elforgatjuk, akkor " + (a * b) + " kisebb négyszög fér el a nagyon.");
-	
+	if (a <= A && b <= B) {
+		alert((Math.floor(A / a) * Math.floor(B / b)) + " kisebb négyszög fér el a nagyon.");
+
+		alert("Ha a lapot 90 fokban elforgatjuk, akkor " + (Math.floor(B / a) * Math.floor(A / b)) + " kisebb négyszög fér el a nagyon.");
+	} else {alert(" A számok legyenek kisebbek a nagy lap méreteinél!");}
 }
 
 /*Adott egy 40cm X 50cm-es papírlap. A program kérjen be két kisebb méretet, és határozza meg, hogy hány darab ilyen kisebb méretű lapot lehet kivágni a nagy lapból. Számoljátok ki ugyanezt, ha a kis lapot elforgatjátok (az oldalak megcserélésével). Például 4X5-ös lapból 100-at lehet kivágni, de ha elforgatjuk 5X4-esre, akkor csak 96-ot*/
---
+++ JSRace/Triangle sides/triangleSides.js
@@ -0,0 +1,17 @@
+﻿function counting() {
+	var a = parseFloat(document.getElementById("numA").value);
+	var b = parseFloat(document.getElementById("numB").value);
+	var c = parseFloat(document.getElementById("numC").value);
+	
+	if ((a + b) <= c || (a + c) <= b  || (b + c) <= a) {
+		alert("A háromszög minden oldalának rövidebbnek kell lennie a másik kettő összegénél. Írj be ennek megfelelő számokat!")
+	} else {
+		var halfDistrict = (a + b + c) / 2;
+		var result = "Az első oldal " + (halfDistrict - a) + " miliméterrel kisebb a kerület felénél.  A második oldal " + (halfDistrict - b) + " miliméterrel kisebb a kerület felénél.  A harmadik oldal " + (halfDistrict - c) + " miliméterrel kisebb a kerület felénél.";
+		alert(result);
+		//alert("A második oldal " + (halfDistrict - b) + " miliméterrel kisebb a kerület felénél.");
+		//alert("A harmadik oldal " + (halfDistrict - c) + " miliméterrel kisebb a kerület felénél.");
+	}
+}
+
+/*	Olvassátok be egy háromszög oldalainak hosszát, majd mindhárom oldalhoz határozzátok meg, hogy mennyivel kisebb a kerület felénél. */
