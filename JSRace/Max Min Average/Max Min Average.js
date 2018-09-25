73527bbd726483efb4efe8d8fefbfa9ad7a13c2f
Szakács Viktor <szavi78@gmail.com>
2018-09-25 18:07:04 +0200

    JS exercises

---
+++ JSRace/Max Min Average/Max Min Average.js
@@ -0,0 +1,31 @@
+﻿function checkNumbers() {
+	var a = parseInt(document.getElementById("numA").value);
+	var b = parseInt(document.getElementById("numB").value);
+	var c = parseInt(document.getElementById("numC").value);	
+	
+	//van-e küztük egyenlő
+	if (a == b || a == c || c== b) {
+		alert('Legyenek különbözőek!')
+	}	else {
+		
+		//legnagyobb
+		if (a > b && a > c) {
+		alert(a + ' a legnagyobb.')
+	} else if (b > a && b > c) {
+		alert(b + ' a legnagyobb.')
+	} else {alert(c + ' a legnagyobb.')}
+		
+		//legkisebb
+		if (a < b && a < c) {
+		alert(a + ' a legkisebb.')
+	} else if (b < a && b < c) {
+		alert(b + ' a legkisebb.')
+	} else {alert(c + ' a legkisebb.')}
+		
+		//átlag
+		alert(((a + b + c) / 3) + ' az átlaguk')
+			
+	}
+}
+
+/* Készítsetek egy programot, amely 3 bekért szám közül meghatározza a legnagyobbat, a legkisebbet, valamint az átlagukat is. */
---
+++ JSRace/Random numbers in array/numInArray.js
@@ -0,0 +1,31 @@
+﻿function counting() {
+	var min = parseInt(document.getElementById("numA").value);
+	var max = parseInt(document.getElementById("numB").value);
+	var randomNums = new Array;
+	
+	if (max <= 0 || min <= 0) {
+		alert('POZITÍV legyen!');
+	} else {
+		if (min >= max) {
+			alert('A minimum legyen KISEBB a maximumnál!');
+		} else {
+	if (max - min <5) {
+		alert('A kettő között legalább ÖT legyen a különbség!');
+	} else {
+		
+		var sum = 0;
+		for (var i = 0; i <= 19; i++) {
+			randomNums[i]=Math.round(Math.random()*(max - min)) + min;
+			sum = sum + randomNums[i];
+		}
+		var randomNumsString = randomNums.join(', ');
+		alert('A következő számokat dobta a gép: ' + randomNumsString);
+		alert('Átlaguk: ' + (sum / 20));
+		alert('Az összegük pedig: ' + sum);
+		
+		}
+	}
+	}
+	}
+
+/*	Készítsetek egy programot, amely egy 20 elemű tömböt feltölt véletlen számokkal. Jelenítsétek meg a generált számokat, majd az összegüket is. */
