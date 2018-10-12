ecefef1bab2d77c4501263ed18031ed2977dbfd4
Szakács Viktor <szavi78@gmail.com>
2018-10-12 21:14:51 +0200

    Add function
    Next picture and previous picture button addid with 2 JS functions.

--- Pic viewer/PicViewer.js
+++ Pic viewer/PicViewer.js
@@ -1,5 +1,36 @@
-function changePic(a) {
+function changePic(a) { //cseréli a nagy képet a kis képre, amire rákattintottak
+	switch(a) {
+		case "http://i.imgur.com/zW5Djxv.jpg":
+			count = 0;
+			break;
+		case "http://i.imgur.com/xjnxvq8.jpg":
+			count = 1;
+			break;
+		case "http://i.imgur.com/eV7ePNo.jpg":
+			count = 2;
+			break;
+		case "http://i.imgur.com/yLZ3GKW.jpg":
+			count = 3;
+			break;
+	}
 	
 	document.getElementById("bigPic").src = a;
+}
 
+
+function nextPic() { //a nagy képet cseréli a következőre, a kövi gomb hívja meg
+	if (count == 3) {
+		count = 0;
+	} else {++count;}
+	
+	document.getElementById("bigPic").src = (pics[count]);
+}
+
+
+function prevPic() { //a nagy képet cseréli az elözőre	
+	if (count == 0) {
+		count = 3;
+	} else {--count;}
+	
+	document.getElementById("bigPic").src = (pics[count]);
 }
